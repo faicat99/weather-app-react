1509a203f5dcc416cceabeae49980d956651627d
Fainche <faicatmck@gmail.com>
2024-08-27 12:08:42 +0100

    added useState for weatherResult and an if statement returning loading in preparation of data from the apiUrl

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -2,23 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function WeatherSearch() {
-        const [city, setCity] = useState();
-        
+        const [city, setCity] = useState("");
+        const [weatherResult, setWeatherResult] = useState(false);
+
+        function displayWeather(response) {
+                setWeatherResult(true);
+                console.log(response.data.main.temp);
+
+        }
         
         function handleSubmit(event) {
                 event.preventDefault();
                 let apiKey="bafb81c036f1dc4bfbb21532bb2ot295"
                 let apiUrl = 
                 `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-                console.log(apiUrl);
-
-
-
+                axios.get(apiUrl).then(displayWeather);
         }
         
         function updateCity(event) {
                 setCity(event.target.value);
         }
+
+        if (weatherResult) {
+                return "Loading..."
+        } else {
 
 
         return (
@@ -27,5 +34,6 @@ export default function WeatherSearch() {
                         <input type="submit" value="Search" />
                  </form>
         );
+}
 }
 
