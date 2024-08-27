2e263d37abcec6468ddca4c0f9c3e3f3ae07c34f
Fainche <faicatmck@gmail.com>
2024-08-27 11:42:47 +0100

    added functions handleSubmit and updateCity for city to be updated in the apiCall

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function WeatherSearch() {
+        const [city, setCity] = useState();
+        
+        
+        function handleSubmit(event) {
+                event.preventDefault();
+                alert(city);
+
+        }
+        
+        function updateCity(event) {
+                setCity(event.target.value);
+        }
+
+
         return (
-                <form>
-                        <input type="search" placeholder="Enter a City..." />
+                <form onSubmit={handleSubmit}>
+                        <input type="search" placeholder="Enter a City..." onChange={updateCity} />
                         <input type="submit" value="Search" />
                  </form>
         );
