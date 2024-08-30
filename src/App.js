f615aa8e3713ee8d0567fe2ae0536b32255166c2
Fainche <faicatmck@gmail.com>
2024-08-30 11:36:44 +0100

    added github and netlify links to project

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <WeatherSearch />
-        <h6>Coded by Fainche Mckeown</h6>
+        <h6>Coded by Fainche Mckeown and is hosted on <a href="https://www.github.com/faicat99">Github</a> and <a href="https://app.netlify.com/sites/weather-app-react-challenge/overview"> Netlify</a></h6>
         </header>
           
     </div>
