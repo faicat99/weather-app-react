566c6d169db662aae3d8876fef414d3e977ed892
Fainche <faicatmck@gmail.com>
2024-08-30 16:06:41 +0100

    removed and updated files

--- src/App.js
+++ src/App.js
@@ -11,9 +11,8 @@ function App() {
       <header className="App-header">
         <h1>Weather App</h1>
         <WeatherSearch />
-        <h6>Coded by Fainche Mckeown and is hosted on <a href="https://www.github.com/faicat99">Github</a> and <a href="https://app.netlify.com/sites/weather-app-react-challenge/overview"> Netlify</a></h6>
+        <h5>Coded by Fainche Mckeown and is hosted on <a href="https://www.github.com/faicat99">Github</a> and <a href="https://app.netlify.com/sites/weather-app-react-challenge/overview"> Netlify</a></h5>
         </header>
-          
     </div>
   );
 }
